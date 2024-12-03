883ad287c8560dc1e2fe3409d82b11c18664da79
bukbukwoski1 <tiagoalencar360@gmail.com>
2024-12-03 10:49:37 -0300

    Correção da Validação do campo B e A

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ document.getElementById('form-numeros').addEventListener('submit', function(even
     const campoA = parseFloat(document.getElementById('campoA').value);
     const campoB = parseFloat(document.getElementById('campoB').value);
 
-    if (campoA > campoB) {
-        alert('Sucesso: O número 1 é maior que o número 2.');
+    if (campoB > campoA) {
+        alert('Sucesso: O número 2 é maior que o número 1.');
     } else {
-        alert('Erro: O número 1 deve ser maior que o número 2.');
+        alert('Erro: O número 2 deve ser maior que o número 1.');
     }
 });
